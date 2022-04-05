5972cb1540b07ed6bfbfb0beb146a1fa6d7a894a
Hananee Bueraheng <96813206+hananeemuhammad@users.noreply.github.com>
2022-04-05 14:14:21 +0800

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -16,53 +16,114 @@ Significant constants
          computation result
 """
 
-while True:
-      try:
-            number_1=int(input("Insert the first number: "))
-            if number_1 == isinstance(number_1, int):
-                  break
-      except ValueError:
-            print('Invalid input, You must enter a number')
-            continue
+import sys
 
-      
-      operation = input('Please type in the math operation you would like to complete: ')
+result = 0
+num_1 = 0
+num_2 = 0
+new_num = 0
+
+def Num1 ():
+    global num_1
+    try:
+        num_1 = float(input('Put your first number : '))
+
+    except ValueError :
+        print('you must enter a number!!')
+        Num1()
+
+def Num2():
+    global num_2
+    try:
+        num_2 = float(input('Put your second number : '))
+    except ValueError :
+            print('you must enter a number!!')
+            Num2 ()
+
+def NewNum ():
+    global new_num
+    try:
+        new_num = float(input('Put new number that your want to calculates : '))
+    
+    except ValueError :
+            print('you must enter a number!!')
+            NewNum ()
+ 
+def Oprt():
+    global result
+    operation = input("Select operator '+' , '-' , '*' , '/' : ")
+    if (operation == '+' or operation == '-' or operation == '*' or operation == '/'):
+        if operation == '+':
+            if result <= 0 :
+                print('{} + {} '.format(num_1, num_2),'= ', "{:.2f}".format(num_1 + num_2))
+                result = num_1 + num_2
+            else :
+                print('{} + {} '.format(result, num_2),'= ', "{:.2f}".format(result + num_2))
+                result = result + num_2
             
-      try:
-            number_2=int(input("Insert the second number: "))
-            if number_2 == isinstance(number_2, int):
-                  break
-      except ValueError:
-            print('Invalid input, You must enter a number')
-            continue
+        elif operation == '-':
+            if result <= 0 :
+                print('{} -{} '.format(num_1, num_2),'= ', "{:.2f}".format(num_1 - num_2))
+                result = num_1 - num_2
+            else :
+                print('{} - {} '.format(result, num_2),'= ', "{:.2f}".format(result - num_2))
+                result = result - num_2
+
+        elif operation == '*':
+            if result <= 0 :
+                print('{} * {} '.format(num_1, num_2),'= ', "{:.2f}".format(num_1 * num_2))
+                result = num_1 * num_2
+            else :
+                print('{} * {} '.format(result, num_2),'= ', "{:.2f}".format(result * num_2))
+                result = result * num_2
+        elif operation == '/':
+            if result <= 0 :
+                print('{} / {} '.format(num_1, num_2),'= ', "{:.2f}".format(num_1 / num_2))
+                result = num_1 / num_2
+            else :
+                print('{} / {} '.format(result, num_2),'= ', "{:.2f}".format(result / num_2))
+                result = result / num_2
+    else :
+        print ("Invalid operator. Try again!!")
+        Oprt ()
+
+def ClearResulat ():
+    global result
+    global num_1
+    global num_2
+    global new_num
+    result = 0
+    num_1 = 0
+    num_2 = 0
+    new_num = 0
+    print (result)
+    Calculation ()
+
+def Calculation ():
+    Num1 ()
+    Num2 ()
+    Oprt ()
+    AddCal ()
+
+def ContinueCal ():
+    NewNum ()
+    Oprt ()
+    AddCal ()
+
+def AddCal ():
+    add = input ("""Add more oparator 'Y' 
+Stop this program 'N'
+Clear result 'cls' : """)
+    
+    if add.upper() == 'Y':
+        ContinueCal ()
+    elif add == "cls" :
+        ClearResulat ()
+    elif add.upper() == 'N' :
+        sys.exit("You're welcome,see you again")
+    else :
+        AddCal ()
+
+Calculation ()
 
 
-      
-
-
-
-      if operation == '+':
-            # print('{} + {} = '.format(number_1, number_2))
-            print('{} + {} = '.format(number_1, number_2) ,(number_1 + number_2))
-
-      elif operation == '-':
-            # print('{} - {} = '.format(number_1, number_2))
-            print('{} - {} = '.format(number_1, number_2) ,(number_1 - number_2))
-
-      elif operation == '*':
-            # print('{} * {} = '.format(number_1, number_2))
-            print('{} * {} = '.format(number_1, number_2) ,(number_1 * number_2))
-
-      elif operation == '/':
-            # print('{} / {} = '.format(number_1, number_2))
-            print('{} / {} = '.format(number_1, number_2) ,(number_1 / number_2))
-
-
-      else:
-            print('You have not typed a valid operator, please run the program again.')
-
-      next_calculation = input("Let's do next calculation? (yes/no): ")
-      if next_calculation == "no":
-          break
-      else :
-            continue
